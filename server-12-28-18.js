30ea614ebdf4d48af90b163695b36e79c5a0a9bd
rkgallaway <rkgallaway@msn.com>
2019-01-08 23:39:07 -0800

    daily warmup

--- server-12-28-18.js
+++ server-12-28-18.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const express = require('express')
+const express = require('express');
 const app = express();
 
 //EJS Template Setup
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
-//static routes
+//static Routes
 app.use( express.static(`${__dirname}/public`));
 
-//app middleware
+//App Middleware//
 app.use(express.json());
 
-//routes
-app.get('/', (req,res) => {
-  res.send(',h1>Hello From /</h1>');
+//Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Hello From</h1>');
 });
 
 app.post('/save', (req, res) => {
@@ -23,16 +23,16 @@ app.post('/save', (req, res) => {
 });
 
 app.get('/err', (req, res, next) => {
-  next('this is a catastrophic error');
+  next('This is a catastrophic error');
 });
 
 app.get('*', (req, res) => {
   res.status(404);
   res.statusMessage = 'Not Found';
-  res.render('not-found', {request:req,});
+  res.render('not-found', {request: req,});
 });
 
-app.use( (err, res, req, next) => {
+app.use( (err, req, res, next) => {
   res.status(500);
   res.statusMessage = 'Server Error';
   res.render('error', {request: req, error: err,});
@@ -40,11 +40,10 @@ app.use( (err, res, req, next) => {
 
 module.exports = {
   server: app,
-  start: (port) => {
+  start: (port) =>{
     app.listen(port, () => console.log('Server up on port', port));
   },
 };
 
-
-
-
+//echo on command line repeats what you said
+// echo '{"name":"John"}' / http post :8080/save
